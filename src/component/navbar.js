c622aaac6a8be567f4c6a59fc3fed72ad1ac10da
Wayne <tsun0227@yahoo.com.tw>
2021-01-25 11:19:46 +0800

    nothing

--- src/component/navbar.js
+++ src/component/navbar.js
@@ -4,11 +4,11 @@ function Navbar(props) {
   return (
     <>
       <div>
-        <a>橙子傳承</a>
-        <a>橙子選擇</a>
-        <a>橙子推薦</a>
-        <a>橙子通告</a>
-        <a>橙子來往</a>
+        <a href="">橙子傳承</a>
+        <a href="">橙子選擇</a>
+        <a href="">橙子推薦</a>
+        <a href="">橙子通告</a>
+        <a href="">橙子來往</a>
       </div>
     </>
   );
