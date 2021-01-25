0c415bed84a9d164349ee2c6bd1df3b059e23056
Wayne <tsun0227@yahoo.com.tw>
2021-01-25 11:09:08 +0800

    navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
-
 import Header from "./component/Header";
+import Navbar from "./component/Navbar";
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Header />
+        <Navbar />
       </header>
     </div>
   );
