b4c282eb34a148e55563d63f592021a2640b0c34
krok1029 <krok1029@gmail.com>
2021-01-25 10:32:35 +0800

    test2

--- src/component/Shop.js
+++ src/component/Shop.js
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function Shop() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div>test1</div>
+    </div>
+  );
 }
 
 export default Shop;
