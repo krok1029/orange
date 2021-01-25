4ced191483b8781fe59a69baf4b906717751abca
krok1029 <krok1029@gmail.com>
2021-01-25 11:11:55 +0800

    footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import "./App.css";
 
 import Header from "./component/Header";
+
+import Footer from "./component/Footer";
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Header />
+
+        <Footer />
       </header>
     </div>
   );
