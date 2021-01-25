c523bc65397c6ccea73f71acade85131330da9ed
krok1029 <krok1029@gmail.com>
2021-01-25 11:05:26 +0800

    create header

---
+++ src/component/Header.js
@@ -0,0 +1,10 @@
+import headerBanner from "../images/header_banner_pc01.png";
+function Header() {
+  return (
+    <div>
+      <img src={headerBanner} alt="banner" />
+    </div>
+  );
+}
+
+export default Header;
