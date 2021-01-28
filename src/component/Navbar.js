ce86b6e2f1f25ae00ff10f56030e74297db529b0
Wayne <tsun0227@yahoo.com.tw>
2021-01-28 09:28:18 +0800

    navbar bcc

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -7,10 +7,11 @@ function Navbar() {
     <>
       <div className="navbar">
         <ul>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
+          <li>橙子傳承</li>
+          <li>橙子選擇</li>
+          <li>橙子推薦</li>
+          <li>橙子通告</li>
+          <li>橙子來往</li>
         </ul>
       </div>
     </>
